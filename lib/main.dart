747c0aaa7e7f7797f87afcd6f0008670b730f38b
Jacob <malin146@umn.edu>
2025-03-12 22:11:15 -0500

    Add velopack

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:velopack_flutter/velopack_flutter.dart';
 import 'package:window_manager_plus/window_manager_plus.dart';
 
 import 'setup/hive_setup.dart';
 import 'setup/window_setup.dart';
 
 void main(List<String> args) async {
+  // Velopack setup
+  await RustLib.init();
+
+  final veloCommands = [
+    '--veloapp-install',
+    '--veloapp-updated',
+    '--veloapp-obsolete',
+    '--veloapp-uninstall',
+  ];
+  if (veloCommands.any((cmd) => args.contains(cmd))) {
+    exit(0);
+  }
+
   WidgetsFlutterBinding.ensureInitialized();
 
   // Hive setup
